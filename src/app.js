7bc354920b3d7099a5644cb5df2a6fd706ad4960
JVelab <jlvela94@gmail.com>
2025-03-30 09:41:04 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  function generarExcusa(arr) {
+    return arr(Math.floor(Math.random() * 4 + 1));
+  }
+
+  let escusa1 = generarExcusa(who);
+  let escusa2 = generarExcusa(action);
+  let escusa3 = generarExcusa(what);
+  let escusa4 = generarExcusa(when);
+
+  console.log(escusa1, escusa2, escusa3, escusa4);
+}
